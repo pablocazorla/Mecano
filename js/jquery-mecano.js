73da2c7ba3f27bda9835291b572ac3d667df654b
pablocazorla <pablo.david.cazorla@gmail.com>
2012-10-15 18:15:39 -0300

    working

--- js/jquery-mecano.js
+++ js/jquery-mecano.js
@@ -5,14 +5,56 @@ $('document').ready(function(){
 		$divCtrlInput = $divCtrl.find('input'),
 		$writeArea = $('#write-area'),
 		$writeInput = $('#write-input'),
+		$btnRecord = $('.btn-record'),
+		$btnReset = $('.btn-reset'),
+		$wpm = $('#wpm'),
 		
 		draggingDivCtrl = false,
 		drawDiv = function(num,win){
 			$divCtrl.css('left',num+'px');
-			$copyArea.css('width',num+'px');
+			$copyArea.css('width',num-40+'px');
 			$writeArea.css('left',num+10+'px');
 			if(!win){$divCtrlInput.val(num)}
-		};
+		},
+		$enterTextCopy = $('#enter-text-copy'),
+		
+		
+		
+		
+		
+		
+		status = 'none',//none,startRec,rec,pause
+		wpm = 0,
+		time = 0,
+		
+		record = function(){
+			status = 'rec';
+			$btnRecord.addClass('btn-recording').html('Recording');
+		},
+		
+		pause = function(){
+			$btnRecord.removeClass('btn-recording').html('Pause');
+			recording = false;
+		},
+		reset = function(){},
+		
+		startRecord = function(){
+			status = 'startRec';
+			var num = 3,
+				tim = setInterval(function(){
+				$wpm.html(num);
+				num--;
+				if(num <= 0){
+					clearInterval(tim);
+					record();
+				}
+			},1000)
+		}
+		
+		
+		
+		
+		;
 		
 	/* START */
 	if($divCtrlInput.val()==''){
@@ -20,20 +62,38 @@ $('document').ready(function(){
 	}else{
 		drawDiv(parseInt($divCtrlInput.val()));
 	}
-		
+	$copyArea.html($enterTextCopy.val());
 	
 	/* EVENTS */
 	$divCtrl.mousedown(function(){
 		draggingDivCtrl = true;
-	});
-	
-	
+	});	
 	$('html,body,#div-ctrl').mousemove(function(event){
 		if(draggingDivCtrl) drawDiv(event.pageX-5);
 	}).mouseup(function(){
 		draggingDivCtrl = false;
+	});	
+	$enterTextCopy.change(function(){
+		$copyArea.html($enterTextCopy.val());
 	});
 	
+	$btnRecord.click(function(event){
+		event.preventDefault();
+		switch(status){
+			case 'none':
+				startRecord();
+				break;
+			case 'startRec':
+				//Nothing
+				break;
+			case 'rec':
+				pause();
+				break;
+			case 'pause':
+				record();
+				break;
+		}
+	});
 	
 	
 	
